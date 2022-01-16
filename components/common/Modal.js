c05bd516510c1416a17bc202fe89197c4dc998e8
waleed zahid <waleed1892@gmail.com>
2022-01-16 18:56:02 +0500

    modal backdrop removed

--- components/common/Modal.js
+++ components/common/Modal.js
@@ -1,5 +1,6 @@
-import {Dialog, Transition} from "@headlessui/react";
-import {Fragment} from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import {XIcon} from "@heroicons/react/solid";
 
 /**
  *
@@ -11,7 +12,7 @@ import {Fragment} from "react";
  * @returns {JSX.Element}
  * @constructor
  */
-export default function Modal({isOpen, close, title, children, size}) {
+export default function Modal({ isOpen, close, title, children, size, hideBackdrop = false }) {
     let sizeClasses = '';
     switch (size) {
         case 'sm':
@@ -30,9 +31,10 @@ export default function Modal({isOpen, close, title, children, size}) {
     return (
         <Transition appear show={isOpen} as={Fragment}>
             <Dialog
+                onClose={() => { }}
+                open={isOpen}
                 as="div"
-                className="fixed inset-0 z-10 bg-gray-800 bg-opacity-20 overflow-y-auto"
-                onClose={close}>
+                className="fixed inset-0 z-50 bg-gray-800 bg-opacity-20 overflow-y-auto">
                 <div className="min-h-screen px-4 flex items-center justify-items-center justify-center">
                     <Transition.Child
                         as={Fragment}
@@ -42,15 +44,15 @@ export default function Modal({isOpen, close, title, children, size}) {
                         leave="ease-in duration-200"
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0">
-                        <Dialog.Overlay className="fixed inset-0"/>
+                        <Dialog.Overlay className="fixed inset-0" />
                     </Transition.Child>
 
                     {/* This element is to trick the browser into centering the modal contents. */}
                     <span
                         className="inline-block h-screen align-middle"
                         aria-hidden="true">
-              &#8203;
-            </span>
+                        &#8203;
+                    </span>
                     <Transition.Child
                         as={Fragment}
                         enter="ease-out duration-300"
@@ -61,11 +63,14 @@ export default function Modal({isOpen, close, title, children, size}) {
                         leaveTo="opacity-0 scale-95">
                         <div
                             className={`inline-block w-full overflow-hidden align-middle transition-all transform bg-white shadow-xl rounded-xl ` + sizeClasses}>
-                            {
-                                title &&
-                                <Dialog.Title as="h3"
-                                              className="text-base px-4 py-2 text-left font-semibold leading-6 text-gray-900 border-b border-gray-200">{title}</Dialog.Title>
-                            }
+                            <div className="border-b border-gray-200 flex justify-between items-center px-4 py-2">
+                                {
+                                    title &&
+                                    <Dialog.Title as="h3"
+                                        className="text-base text-left font-semibold leading-6 text-gray-900">{title}</Dialog.Title>
+                                }
+                                <XIcon onClick={close} className="w-4 h-4 text-red-500 cursor-pointer"/>
+                            </div>
                             <div className={`p-4 max-h-860-px overflow-auto`}>
                                 {children}
                             </div>
